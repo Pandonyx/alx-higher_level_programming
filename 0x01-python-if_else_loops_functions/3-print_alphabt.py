2473ea00b1c04101c15c68a85c553b33a6ef38ba
pandonyx <pandonyx#gmail.com>
2023-06-06 10:31:42 -0700

    task3 fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) not in ('q', 'e'):
-        print(chr(i), end='')
+for code in range(ord('a'), ord('z') + 1):
+    if code == ord('e') or code == ord('q'):
+        continue
+    print("{:c}".format(code), sep='', end='')
